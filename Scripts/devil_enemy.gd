f6dd0cc154d8b057d3bd43f9144719459947469e
Anh La <anh.la@bellevuecollege.edu>
2024-05-09 18:28:29 -0700

    Added enemies

---
+++ Scripts/devil_enemy.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const SPEED = 60
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+
+
+var direction = 1
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1
+	if ray_cast_left.is_colliding():
+		direction = 1
+	position.x += direction * SPEED * delta
